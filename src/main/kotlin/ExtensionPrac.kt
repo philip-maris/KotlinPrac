48ae0a18af2713b683c39ca2eeb298f2c0cfefbc
philip-maris <ezeoforphillip@gmail.com>
2022-05-05 13:54:52 +0100

    first commit

---
+++ src/main/kotlin/ExtensionPrac.kt
@@ -0,0 +1,14 @@
+/*
+fun main() {
+    
+    val string = "the day"
+    string.separateLetters()
+}
+
+fun String.separateLetters(){
+
+    for( i in this){
+        println(i)
+    }
+
+}*/
---
+++ src/main/kotlin/JsonPrac.kt
@@ -0,0 +1,39 @@
+import com.google.gson.Gson
+import java.io.FileReader
+
+/*data class Person(var name: String, var age: Int, var birthday: Birthday, var address: Address)
+data class Address(var country: String, var state: String)
+data class Birthday(var day: Int, var month: Int, var year: Int)
+
+
+fun main(){
+    val newManAddress = Address(" Nigeria ", " lagos")
+    val newManBirthday = Birthday(4, 4, 1990)
+
+    val newMan = Person("john philips", 21, newManBirthday, newManAddress)
+    var woman = newMan.copy()
+    woman.name = "mary jane"
+
+    var gson = Gson()
+
+    var people = arrayOf(newMan, woman)
+
+    // from pojo to Json
+    var peopleInJsonFormat = gson.toJson(people)
+    println(peopleInJsonFormat)
+
+    var newPeople : Array<Person> = gson.fromJson(peopleInJsonFormat , Array<Person> :: class.java)
+    println("\n  Back to pojo format")
+    for(i in newPeople) println(i)
+}*/
+
+
+data class Movie(var name: String, var watchDate: String,var provider: String ,var seriesNo: Int,var episodeNo: String)
+
+fun main (){
+
+    val gson = Gson()
+
+    val movieList: Array<Movie> = gson.fromJson(FileReader("./src/main/resources/Prac.json"), Array<Movie>::class.java)
+   for (movie in movieList) println("\n $movie")
+}
